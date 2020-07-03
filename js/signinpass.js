515df37b7291e3722d1b9afa552519837b1963a0
Pradeep <rpradeep3696@gmail.com>
2020-07-03 12:35:09 +0530

    Initial commit

---
+++ js/signinpass.js
@@ -0,0 +1,57 @@
+﻿$(function () {  
+   $('#homestep2').css('display', 'none');
+   $('#homestep3').css('display', 'none');
+   $('#homestep4').css('display', 'none');
+   $('#homestep5').css('display', 'none');
+   $('#homestep6').css('display', 'none');
+   $('#homestep7').css('display', 'none');   
+});
+
+function openhomestep2() {
+   $('#homestep1').css('display', 'none');
+   $('#homestep2').css('display', 'block');
+}
+function closehomestep2() {
+   $('#homestep1').css('display', 'block');
+   $('#homestep2').css('display', 'none');
+}
+function openhomestep3() {
+   $('#homestep2').css('display', 'none');
+   $('#homestep3').css('display', 'block');
+}
+function closehomestep3() {
+   $('#homestep2').css('display', 'block');
+   $('#homestep3').css('display', 'none');
+}
+function openhomestep4() {
+   $('#homestep3').css('display', 'none');
+   $('#homestep4').css('display', 'block');
+}
+function closehomestep4() {
+   $('#homestep3').css('display', 'block');
+   $('#homestep4').css('display', 'none');
+}
+function openhomestep5() {
+   $('#homestep4').css('display', 'none');
+   $('#homestep5').css('display', 'block');
+}
+function closehomestep5() {
+   $('#homestep4').css('display', 'block');
+   $('#homestep5').css('display', 'none');
+}
+function openhomestep6() {
+   $('#homestep5').css('display', 'none');
+   $('#homestep6').css('display', 'block');
+}
+function closehomestep6() {
+   $('#homestep5').css('display', 'block');
+   $('#homestep6').css('display', 'none');
+}
+function openhomestep7() {
+   $('#homestep6').css('display', 'none');
+   $('#homestep7').css('display', 'block');
+}
+function closehomestep7() {
+   $('#homestep6').css('display', 'block');
+   $('#homestep7').css('display', 'none');
+}
